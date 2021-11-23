d02b9937aa91ce71bb3708d1de8b2a921207ec1c
Sumanth Peddamatham <peddamat@gmail.com>
2021-11-22 19:41:04 -0800

    Wrapped each line with an href so links are clickable.

--- files/create-sitemap.sh
+++ files/create-sitemap.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+SITE='https://target-data.nci.nih.gov'
+
 find /root/mirror -type f -iname "*.html" -exec urlextract -u -l 0 {} \; \
     | grep AML \
     | grep -v '?' \
     | sort \
-    | uniq > /root/output/site-contents.html
+    | uniq \
+    | while read url; do echo "<a href='$url'>${url#$SITE}</a><br />"; done \
+    | tee /root/output/site-contents.html
